fc777cc067219aadbe2a0119187c80a072d2b758
Hachem-H <personal@hachem.cc>
2024-03-19 22:00:35 +0200

    Screw react, svelte+threlt ftw

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { Canvas } from "@threlte/core";
+    import Scene from "./BackgroundScene.svelte";
+</script>
+
+<div style="width:100vw; height:100vh; position: absolute;">
+    <Canvas>
+        <Scene />
+    </Canvas>
+</div>
+<div style="position:fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); color: white">
+    <h1>About me</h1>
+</div>
